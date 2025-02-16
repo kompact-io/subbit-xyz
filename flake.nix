5760196f3e99888b1d668a9c7eeac17e4b39206d
waalge <waagle@jes>
2025-02-16 21:45:45 +0000

    subbit.xyz, restarted

--- flake.nix
+++ flake.nix
@@ -1,50 +1,69 @@
 {
-  description = "Kompact project";
+  description = "Subbit.xyz : Cardano's featherweight L2";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pre-commit-hooks-nix.url = "github:hercules-ci/pre-commit-hooks.nix/flakeModule";
-    pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
-    aiken.url = "github:aiken-lang/aiken";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    git-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    aiken.url = "github:waalge/aiken/waalge/fix-nix-build";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; }
-      {
-        imports = [
-          inputs.pre-commit-hooks-nix.flakeModule
-          inputs.treefmt-nix.flakeModule
-        ];
-        systems = [ "x86_64-linux" "aarch64-darwin" ];
-        perSystem = { config, self', inputs', pkgs, ... }: {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            flakeFormatter = true;
-            programs = {
-              prettier = {
-                enable = true;
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;}
+    {
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = ["x86_64-linux" "aarch64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          flakeFormatter = true;
+          programs = {
+            prettier = {
+              enable = true;
+              settings = {
+                printWidth = 80;
+                proseWrap = "always";
               };
             };
-          };
-
-          devShells.default =
-          pkgs.mkShell {
-            nativeBuildInputs = [
-              config.treefmt.build.wrapper
-            ]
-            ;
-            shellHook = ''
-              echo 1>&2 "Welcome to the development shell!"
-            '';
-            name = "hello-aiken";
-            packages = with pkgs; [
-              inputs'.aiken.packages.aiken
-              pandoc
-            ];
+            alejandra.enable = true;
           };
         };
-        flake = { };
+        pre-commit.settings.hooks = {
+          treefmt.enable = true;
+          aiken = {
+            enable = true;
+            name = "aiken";
+            description = "Run aiken's formatter on ./aik";
+            files = "\\.ak";
+            entry = "${inputs'.aiken.packages.aiken}/bin/aiken fmt ./aik";
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            config.treefmt.build.wrapper
+          ];
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+            echo 1>&2 "Welcome to the development shell!"
+          '';
+          packages = [
+            inputs'.aiken.packages.aiken
+          ];
+        };
       };
+      flake = {};
+    };
 }
